e0d2c9037b3aecf02c73d8e8e1a58a21f47b5f5c
Xiaorong-Shan <89878355+Xiaorong-Shan@users.noreply.github.com>
2023-06-11 16:40:53 -0400

    Add files via upload

---
+++ R_Code/01_hysplit_pp_1951_array.R
@@ -0,0 +1,187 @@
+# srun -p test --mem 15g -t 0-01:00 -c 1 -N 1 --pty /bin/bash
+
+library(disperseR) # our package
+library(ncdf4)
+library(data.table)
+library(tidyverse)
+library(parallel)
+library(sf)
+library(viridis)
+library(ggplot2)
+library(scales)
+library(ggsn)
+library(gridExtra)
+library(ggmap)
+library(ggrepel)
+library(fst)
+library( USAboundariesData)
+
+disperseR::create_dirs('/projects/HAQ_LAB/xshan2/disperseR_Linux')
+
+proc_dir <- '/scratch/xshan2/disperseR_Linux/main/process'
+
+# define a coordinate reference system
+p4s <- "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m"
+
+years.run <- 1951
+
+# download data
+disperseR::get_data(data = "all",
+                    start.year = "1951",
+                    start.month = "01",
+                    end.year = "1951",
+                    end.month = "12")
+
+#read the facilities' locations                    
+unit.facility <- read.csv("/scratch/xshan2/R_Code/disperseR/generator1940_gen.csv")                    
+
+unit.facility <- as.data.table(unit.facility)
+
+unit.facility.c <- dcast( unit.facility,
+              X + Y ~ variable,
+              fun.aggregate = sum,
+              value.var = 'value',
+              na.rm = TRUE)
+
+unit.facility.c$Height <- 374.17
+unit.facility.c$year <- 1951
+unit.facility.c$uID <- 1:nrow(unit.facility.c)
+unit.facility.c$ID <- 1:nrow(unit.facility.c)
+unit.facility.c$uID <- as.character(unit.facility.c$uID)
+unit.facility.c$ID <- as.character(unit.facility.c$ID)
+
+# X --> longitude, Y --> latitude
+colnames(unit.facility.c)[1] <- "Longitude"
+colnames(unit.facility.c)[2] <- "Latitude"
+
+input_refs <- disperseR::define_inputs(units = unit.facility.c,
+                                       startday = '1951-01-11',
+                                       endday = '1951-12-31',
+                                       start.hours =  12,
+                                       duration = 24 * 7)
+
+# select specific 
+array_num <- as.numeric( Sys.getenv("SLURM_ARRAY_TASK_ID"))
+array_num <- ifelse( array_num == '' | is.na( array_num), 1, array_num)
+
+# total array numbers
+numbers <- 100
+# Calculate the number of tasks and determine the index for each task
+total_tasks <- nrow(input_refs)
+tasks_per_job <- ceiling(total_tasks / numbers)
+start_index <- (array_num - 1) * tasks_per_job + 1
+end_index <- min(array_num * tasks_per_job, total_tasks)
+
+set.seed( array_num)
+
+#print(start_index:end_index)
+
+#print(input_refs[start_index:end_index])
+
+hysp_raw <- disperseR::run_disperser_parallel(input.refs = input_refs[start_index:end_index],
+                                              pbl.height = pblheight,
+                                              species = 'so2',
+                                              proc_dir = proc_dir,
+                                              overwrite = TRUE, ## FALSE BY DEFAULT
+                                              npart = 100,
+                                              keep.hysplit.files = FALSE,
+                                              mc.cores=1) #parallel::detectCores())
+
+
+# hysp_raw <- disperseR::run_disperser_parallel(input.refs = input_refs[c( array_num, sample( 1:nrow( unitsuse)))],
+#                                               pbl.height = pblheight,
+#                                               species = 'so2',
+#                                               proc_dir = proc_dir,
+#                                               overwrite = TRUE, ## FALSE BY DEFAULT
+#                                               npart = 100,
+#                                               keep.hysplit.files = FALSE,
+#                                               mc.cores=1) #parallel::detectCores())
+                                              
+                                              
+
+# yearmons <- disperseR::get_yearmon(start.year = as( years.run[1], 'character'),
+#                                     start.month = "01",
+#                                     end.year = as( years.run[length( years.run)], 'character'),
+#                                     end.month = '02')
+
+
+# unitsuse <- units.CAMP[!duplicated(ID)]
+# 
+# linked_grid <- disperseR::link_all_units(units.run = unitsuse[c( array_num, sample( 1:nrow( unitsuse)))],
+#                                          link.to = 'grids',
+#                                          mc.cores = 1,
+#                                          year.mons = yearmons,
+#                                          pbl.height = pblheight,
+#                                          crosswalk. = crosswalk,
+#                                          duration.run.hours = 24*7,
+#                                          res.link = 36000,
+#                                          overwrite = TRUE,
+#                                          pbl.trim = FALSE,
+# 					                     crop.usa = TRUE,
+#                                          return.linked.data = TRUE)
+# 
+# unique( linked_grid$month)
+# 
+# #Combine all results into RData file
+# combined_gridlinks <- disperseR::combine_daily_links(
+#   month_YYYYMMs = yearmons,
+#   link.to = 'grids',
+#   filename = 'hyads_vig_unwgted_grids.RData')
+# 
+# 
+# 
+# exp_day_unit_grids <- disperseR::calculate_daily_exposure(
+#   year.E = 1951,
+#   year.D = 1951,
+#   link.to = 'grids',
+#   pollutant = 'SO2.tons',
+#   rda_file = file.path(rdata_dir, "hyads_vig_unwgted_grids.RData"),
+#   exp_dir = exp_dir,
+#   units.mo = PP.units,
+#   source.agg = 'unit',
+#   return.daily.data = T)
+# 
+# unique(exp_day_unit_grids$yearmonth)
+# 
+# exp_day_unit_grids.c <- dcast( exp_day_unit_grids,
+#                         x + y ~ yearmonth, 
+#                         fun.aggregate = sum,
+#                         value.var = 'hyads',
+#                         na.rm = TRUE)
+# 
+# # create raster object
+# campfire_day.r <- rasterFromXYZ( exp_day_unit_grids.c,
+#                                  crs = "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m")
+# 
+# 
+# 
+# # create sf polygon object
+# campfire_day.sp <- rasterToPolygons( campfire_day.r)
+# campfire_day.sf <- st_as_sf( campfire_day.sp)
+# 
+# 
+# 
+# # melt back to long format
+# campfire_day.sf.m <-
+#   as.data.table( campfire_day.sf) %>%
+#   melt( id.vars = 'geometry',
+#         variable.name = 'month',
+#         value.name = 'N')
+# 
+# 
+# 
+# # NA for disperseR means 0
+# campfire_day.sf.m[is.na( N), N := 0]
+# 
+# # download some US data
+# states <- us_states()
+# 
+# campfire_day.sf.m$geometry <-
+#   st_transform( campfire_day.sf.m$geometry,
+#                 crs = st_crs( states))
+# 
+# 
+
+
+
+
---
+++ R_Code/EIA_facility.R
@@ -0,0 +1,237 @@
+library( ggplot2)
+library( data.table)
+library( viridis)
+library( tidyverse)
+library( sf)
+
+# d_ampd2 <- fread( '~/Dropbox/Harvard/ARP/Data_AMPD_EIA/AMPD_Unit_with_Sulfur_Content_and_Regulations_with_Facility_Attributes.csv')
+# 
+# ampd_unique <- unique( d_ampd2[Initial.Year.of.Operation <= 1940,
+#                                .( Facility.ID..ORISPL., Unit.ID,
+#                                   Initial.Year.of.Operation,
+#                                   Facility.Name.x, State.x, County.x,
+#                                   Facility.Latitude.x, Facility.Longitude.x,
+#                                   Source.Category.x)])
+# fwrite( ampd_unique, '~/Dropbox/GeorgeMason/Grants/2020/NIH_1940s_PP/prelim_analysis/ampd_coal_plants.csv')
+# 
+# 
+# # plot facilities by initial operating year
+# ggplot( d_ampd2[Initial.Year.of.Operation <= 1940]) +
+#   geom_polygon(data = map_data("state"),
+#                aes( x = long,
+#                     y = lat,
+#                     group = group),
+#                fill = 'white',
+#                color = "black",
+#                size = .25) +
+#   geom_point( aes( x = Facility.Longitude.x,
+#                    y = Facility.Latitude.x,
+#                    color = Initial.Year.of.Operation)) +
+#   scale_color_viridis( option = 'A',
+#                        end = .8)
+
+generator2010ret <- readxl::read_xls( 'c:/Users/xshan2/OneDrive - George Mason University - O365 Production/GMU_PhD/01_Research/02_2019fall_ADRD/prelim_analysis/PowerPlant/GeneratorsY2010.xls',
+                                      sheet = 3) %>% data.table
+generator2010act <- readxl::read_xls( 'c:/Users/xshan2/OneDrive - George Mason University - O365 Production/GMU_PhD/01_Research/02_2019fall_ADRD/prelim_analysis/PowerPlant/GeneratorsY2010.xls',
+                                      sheet = 1) %>% data.table
+
+# limit to facilities operating by 1940 and not retired by 1940
+cols_keep <- c( "UTILITY_ID", "PLANT_CODE", "PLANT_NAME", "STATE", "COUNTY",
+                "NAMEPLATE", "OPERATING_YEAR", "GENERATOR_ID", #measure of the generator capacity NAMEPLATE, EIA 860, annual average
+                "ENERGY_SOURCE_1", "ENERGY_SOURCE_2")
+
+generator1940ret <- generator2010ret[OPERATING_YEAR <= 1940 & RETIREMENT_YEAR > 1940, ..cols_keep]
+generator1940act <- generator2010act[OPERATING_YEAR <= 1940, ..cols_keep]
+generator1940 <- rbind( generator1940ret, generator1940act)
+
+
+# plot total nameplate capacity by county
+generator1940[ (ENERGY_SOURCE_1 %in% c( 'BIT', 'ANT', 'LIG', 'SUB', 'WC', 'RC')),
+               Coal := NAMEPLATE]
+generator1940[ (ENERGY_SOURCE_1 %in% c( 'DFO', 'JF', 'KER', 'PC', 'RFO', 'WO')),
+               Petroleum := NAMEPLATE]
+generator1940[ (ENERGY_SOURCE_1 %in% c( 'BFG', 'NG', 'OG', 'PG', 'SG', 'SGC')),
+               Gas := NAMEPLATE]
+generator1940[ (ENERGY_SOURCE_1 %in% c( 'AB', 'MSW', 'OBS', 'WDS', 'OBL', 'SLW', 'BLQ', 'WDL', 'LFG', 'OBG')),
+               Renewable := NAMEPLATE]
+
+# sum by fuel type and county
+fueltype_names <- c( 'Coal', 'Petroleum', 'Gas', 'Renewable')
+
+generator1940_county <- generator1940[, lapply( .SD, sum, na.rm = T),
+                                      .SDcols = fueltype_names, by = .( STATE, COUNTY)]
+
+# plot
+counties.sf <- USAboundaries::us_counties( )
+states48 <- state.abb[!(state.abb %in% c( 'HI', 'AK'))]
+counties.sf <- counties.sf[ counties.sf$state_abbr %in% states48,]
+counties.sf$name <- tolower( counties.sf$name)
+generator1940_county[, COUNTY := tolower( COUNTY)]
+generator1940_county.sf <- merge( generator1940_county,
+                                  counties.sf[, c( 'state_abbr', 'name', 'geometry')], 
+                                  by.x = c( 'STATE', 'COUNTY'),
+                                  by.y = c( 'state_abbr', 'name'),
+                                  all.y = T)
+
+# melt
+generator1940_county.sf.m <- melt( generator1940_county.sf,
+                                   id.vars = c( 'STATE', 'COUNTY', 'geometry'),
+                                   measure.vars = fueltype_names)
+generator1940_county.sf.m[is.na( value), value := 0]
+# us states dataset
+states48 <- state.abb[!(state.abb %in% c( 'HI', 'AK'))]
+states_raw <- USAboundaries::us_states()
+us_states_raw <- states_raw[ states_raw$state_abbr %in% states48,]
+
+gg_facility_locs <- 
+  ggplot( generator1940_county.sf.m[ STATE %in% states48 & 
+                                       !( variable %in% 'Renewable')]) + 
+  geom_sf( data = us_states_raw, fill = NA) +
+  geom_sf( aes( fill = value, geometry = geometry), color = NA, size = 0) + 
+  scale_fill_gradient( low = 'azure', high = 'blueviolet', limits = c( 0, 40),
+                       breaks = c( 0, 3, 10, 30),
+                       oob = scales::squish,
+                       trans = scales::pseudo_log_trans()) + 
+  facet_wrap( . ~ variable, nrow = 1) +
+  labs( fill = bquote( atop( 'Capacity', '(MW)'))) +
+  theme_minimal() + 
+  theme( axis.text = element_blank(),
+         legend.direction = 'vertical',
+         legend.position = 'right',
+         legend.text = element_text( size = 12),
+         legend.title = element_text( size = 10),
+         strip.text = element_text( size = 14),
+         panel.grid = element_blank())
+
+ggsave( gg_facility_locs,
+        filename = '~/Dropbox/GeorgeMason/Grants/2020/NIH_1940s_PP/prelim_analysis/facility_locs_1940.png',
+        width = 3.5*3, height = 2.25, scale = 1.2)
+
+
+generator2040_coal <- generator1940[!is.na( Coal)]
+fwrite( generator2040_coal, '~/Dropbox/GeorgeMason/Grants/2020/NIH_1940s_PP/prelim_analysis/eia_coal_plants.csv')
+
+
+
+nrow( generator2040_coal)
+nrow( generator2040_pet)
+nrow( generator2040_gas)
+nrow( generator2040_ab)
+nrow( generator2040_lrb)
+nrow( generator2040_grb)
+
+# plot counties by total nameplate capacity for rach fuel type
+length( unique( generator2040_coal$COUNTY))
+length( unique( generator2040_pet$COUNTY))
+length( unique( generator2040_gas$COUNTY))
+
+## ============================================== ##
+## create inverse distance weighted metrics
+## ============================================== ##
+# define a coordinate reference system
+p4s <- "+proj=lcc +lat_1=33 +lat_2=45 +lat_0=40 +lon_0=-97 +a=6370000 +b=6370000"
+
+generator1940_gen <- generator1940[, lapply( .SD, sum, na.rm = T),
+                                   .SDcols = fueltype_names, 
+                                   by = .( UTILITY_ID, PLANT_CODE, PLANT_NAME, STATE, COUNTY)]
+generator1940_gen[, COUNTY := tolower( COUNTY)]
+generator1940_gen.sf <- merge( generator1940_gen,
+                               counties.sf[, c( 'state_abbr', 'name', 'geometry')], 
+                               by.x = c( 'STATE', 'COUNTY'),
+                               by.y = c( 'state_abbr', 'name'))
+generator1940_gen.sf[ , geometry := st_transform( geometry, p4s)]
+
+
+# melt to long form
+generator1940_gen.sf.m <- 
+  melt( generator1940_gen.sf,
+        id.vars = c( 'UTILITY_ID', 'PLANT_CODE', 'PLANT_NAME', 'STATE', 'COUNTY', 'geometry'))
+
+# assign each facility the centroid of its county because we don't have plant-specific coordinates
+generator1940_gen.sf.m[, centroid_geo := st_centroid( geometry)] 
+
+# limit to just capacity > 0
+generator1940_gen_use <- generator1940_gen.sf.m[value > 0]
+
+# function to calculate inverse-distance weighting for each facility
+inv_distancer <- function( n, 
+                           p.sf = generator1940_gen_use, 
+                           grid.sf = counties.sf){
+  print( scales::percent( n / nrow( p.sf)))
+  # transform crs
+  grid.sf <- st_transform( grid.sf, st_crs( p.sf$centroid_geo))
+  
+  # calculate distances
+  site_dist <- as.vector( 
+    st_distance(p.sf[n,centroid_geo], 
+                st_centroid( grid.sf))) #/ 1e3
+  
+  # distance is 1 in same grid cell
+  site_dist[site_dist == 0] <- min( site_dist[ site_dist > 0])
+  
+  # output datset
+  out <- data.table( N = n,
+                     p.sf[n, .( UTILITY_ID, PLANT_CODE, PLANT_NAME, STATE, COUNTY, variable, value)],
+                     grid.sf[, c( 'statefp', 'countyfp', 'countyns', 'geoid', 'state_name', 'state_abbr')],
+                     inv_dist = 1 / site_dist)
+  
+  out[ , exp_inv_dist := value * inv_dist]
+  
+  return( out)
+}
+
+# do the calculation
+exp_inverse_dist <- lapply( 1:nrow( generator1940_gen_use),
+                            inv_distancer) %>%
+  rbindlist
+
+setnames( exp_inverse_dist,
+          c( 'variable', 'value'),
+          c( 'FUEL', 'NAMEPLATE_CAPACITY'))
+
+counties.sf <- counties.sf[,-9] #remove duplicate name
+
+# plot exposure from each
+exp_inverse_dist_sum <- 
+  exp_inverse_dist[, .( exposure = sum( exp_inv_dist)),
+                   by = .( FUEL, geoid)] %>%
+  merge( counties.sf, by = 'geoid')
+
+exp_inverse_dist_f <- 
+  ggplot( exp_inverse_dist_sum,
+          aes( geometry = geometry,
+               fill = exposure)) + 
+  # geom_sf( data = counties.sf, color = 'black', fill = 'white') + 
+  geom_sf( color = NA, ) + 
+  scale_fill_gradient( low = 'white', high = 'red') + 
+  facet_wrap( . ~ FUEL)
+
+ggsave( exp_inverse_dist_f,
+        filename = '/Users/xshan2/OneDrive - George Mason University - O365 Production/GMU_PhD/01_Research/02_2019fall_ADRD/prelim_analysis/PowerPlant/exp_inverse_dist_facility_1940.png',
+        width = 3.5*2, height = 2.25*2, scale = 1.2)
+
+save( exp_inverse_dist,
+      exp_inverse_dist_sum,
+      file =  '~/Dropbox/GeorgeMason/Grants/2020/NIH_1940s_PP/prelim_analysis/inv_dist_facilities.RData')
+
+## ============================================== ##
+## check out CMIP6 gridded emissions
+## ============================================== ##
+library( raster)
+library( ncdf4)
+
+# https://handle-esgf.dkrz.de/lp/21.14100/5b2e3831-1d4d-4566-ab60-d5f1cda1a73b
+file_in <- '~/Dropbox/Harvard/Meetings_and_People/JoanCasey/1940s_PowerPlants/SO2-em-anthro_input4MIPs_emissions_CMIP_CEDS-2017-05-18_gn_190001-194912.nc'
+
+emiss <- raster::brick( file_in, varname = "SO2_em_anthro",
+                        lvar = 3, level = 5)
+
+emiss1940 <- emiss[['X1940.07.15']]
+emiss1940[emiss1940 == 0] <- NA
+plot( emiss1940, xlim = c( -180, -40), ylim = c( 0, 90))
+plot( emiss[['X1949.12.16']])
+sum( values( emiss[['X1940.01.16']]))
+
+ncin <- nc_open(file_in)
+names( ncin$var)
+names( ncin$dim)
