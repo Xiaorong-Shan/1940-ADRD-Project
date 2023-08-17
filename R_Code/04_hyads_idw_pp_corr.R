1242ab9c1685247d6e1b33b158cfee941c1c3a5e
Xiaorong-Shan <89878355+Xiaorong-Shan@users.noreply.github.com>
2023-08-17 14:49:49 -0400

    Update 04_hyads_idw_pp_corr.R

--- R_Code/04_hyads_idw_pp_corr.R
+++ R_Code/04_hyads_idw_pp_corr.R
@@ -1,3 +1,11 @@
+library(ggplot2)
+library(USAboundariesData)
+library(pryr)
+library(fst)
+library(data.table)
+library(sf)
+library(viridis)
+library(raster)
 
 #read the summed 10-year hyads exposure, summed coal, petroleum, gas and reneable together for the same county
 pp_exposure.m <- st_read("/home/xshan2/HAQ_LAB/xshan2/R_Code/powerplant/pp_hyads_shp/pp_exposure_m.shp")
